a60b8719b9bd759324f994ba6b3d6fc57cbd0896
namancg <naman.c@perfios.com>
2021-08-31 20:11:54 +0530

    REFACTORED: used insert with key method

--- src/com/bridgelabz/linkedlists/LinkedListMain.java
+++ src/com/bridgelabz/linkedlists/LinkedListMain.java
@@ -5,23 +5,27 @@ public class LinkedListMain {
 		LinkedList<Integer> firstNode = new LinkedList<Integer>(56);
 		LinkedList<Integer> secondNode = new LinkedList<Integer>(30);
 		LinkedList<Integer> thirdNode = new LinkedList<Integer>(70);
+		LinkedList<Integer> nodeToBeInserted = new LinkedList<Integer>(40);
 		LinkedListImpl myLinkedList= new LinkedListImpl();
+		int node=30;
 		myLinkedList.add(firstNode);
 		//myLinkedList.add(secondNode);
 		myLinkedList.append(thirdNode);
 		myLinkedList.insert(secondNode, firstNode);
-		myLinkedList.printLinkedList();
+		//myLinkedList.printLinkedList();
 		INode delete=myLinkedList.deleteLast();
-		System.out.println("AFTER DELETING:");
-		myLinkedList.printLinkedList();
+		//System.out.println("AFTER DELETING:");
+		//myLinkedList.printLinkedList();
 		
-		if(myLinkedList.search(70)==true)
+		/*if(myLinkedList.search(node) != null)
 		{
 			System.out.println("FOUND");
 		}
 		else
-			System.out.println("NOT FOUND");
+			System.out.println("NOT FOUND");*/
 		
+		myLinkedList.insertWithKeyValue(node, nodeToBeInserted);
+		myLinkedList.printLinkedList();
 	
 }
 }
