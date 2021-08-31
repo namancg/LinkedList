5fd3b276df2d8f5dfa0e4ec81708b3b9924d6b3b
namancg <naman.c@perfios.com>
2021-08-31 18:05:10 +0530

    REFACTORED: Insert method used

--- src/com/bridgelabz/linkedlists/LinkedListMain.java
+++ src/com/bridgelabz/linkedlists/LinkedListMain.java
@@ -2,22 +2,15 @@ package com.bridgelabz.linkedlists;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		LinkedList<Integer> firstNode = new LinkedList<Integer>(70);
+		LinkedList<Integer> firstNode = new LinkedList<Integer>(56);
 		LinkedList<Integer> secondNode = new LinkedList<Integer>(30);
-		LinkedList<Integer> thirdNode = new LinkedList<Integer>(56);
+		LinkedList<Integer> thirdNode = new LinkedList<Integer>(70);
 		LinkedListImpl myLinkedList= new LinkedListImpl();
 		myLinkedList.add(firstNode);
-		myLinkedList.add(secondNode);
-		myLinkedList.add(thirdNode);
+		//myLinkedList.add(secondNode);
+		myLinkedList.append(thirdNode);
+		myLinkedList.insert(secondNode, firstNode);
 		myLinkedList.printLinkedList();
-		
-		LinkedList<Integer> Node1 = new LinkedList<Integer>(56);
-		LinkedList<Integer> Node2 = new LinkedList<Integer>(30);
-		LinkedList<Integer> Node3 = new LinkedList<Integer>(70);
-		LinkedListImpl myLinkedList1= new LinkedListImpl();
-		myLinkedList1.append(Node1);
-		myLinkedList1.append(Node2);
-		myLinkedList1.append(Node3);
-		myLinkedList1.printLinkedList();
-	}
+	
+}
 }
