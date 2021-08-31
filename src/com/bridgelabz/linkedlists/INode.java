00ae053fd4281bb13834f99f8e583e0b511d963f
namancg <naman.c@perfios.com>
2021-08-31 17:02:58 +0530

    ADDED: interface for performing node operations

--- src/com/bridgelabz/linkedlists/INode.java
+++ src/com/bridgelabz/linkedlists/INode.java
@@ -3,5 +3,6 @@ package com.bridgelabz.linkedlists;
 public interface INode<K> {
 	public K getKey();
 	public void setKey(K key);
-	INode <K> getNextNode();
+	INode getNextNode();
+	public void setNextNode(INode next);
 }
